4166527646fa8e6dd91bc8730d8362e34291e995
faanzylosttt <andrian000062@gmail.com>
2025-06-26 19:04:17 +0700

    Update dbv1.js

--- api/dbv1.js
+++ api/dbv1.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).send("Only GET allowed");
 
-  const repo = "databasescript/databasev10";
-  const filename = "databasev10";
+  const repo = "faanzylosttt/DBV1";
+  const filename = "DBV1.json";
   const token = process.env.GITHUB_TOKEN;
 
   try {
